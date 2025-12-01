4b7b610e7ce050b3146794ebde061a421d7c4845
duy10102003 <duya1k20nl4@gmail.com>
2025-12-01 17:23:29 +0700

    base_backend

---
+++ SWP.Core/GMSAttribute/GMSTableNameAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP.Core.GMSAttribute
+{
+    /// <summary>
+    /// Setup mapping tên bảng trong database
+    /// Created by: DuyLC(01/12/2025)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class GMSTableNameAttribute : Attribute
+    {
+        public string TableName { get; set; }
+        public GMSTableNameAttribute(string tableName)
+        {
+               TableName = tableName;  
+        }
+    }
+}
